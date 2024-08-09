8dea67315f96554722f62ba19b0cc3c63e7214b5
Olexander <v.h.olexander@gmail.com>
2024-08-09 13:01:22 +0300

    make build

---
+++ index.91195c0a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");e.style.cursor="pointer",e.addEventListener("click",function(e){var t=e.target.closest("li");t&&(""===t.firstElementChild.style.display?t.firstElementChild.style.display="none":(t.firstElementChild.style.display="",t.firstElementChild.removeAttribute("style")))});
+//# sourceMappingURL=index.91195c0a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const tree = document.querySelector(".tree");
+tree.style.cursor = "pointer";
+tree.addEventListener("click", (e)=>{
+    const li = e.target.closest("li");
+    if (li) {
+        if (li.firstElementChild.style.display === "") li.firstElementChild.style.display = "none";
+        else {
+            li.firstElementChild.style.display = "";
+            li.firstElementChild.removeAttribute("style");
+        }
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
